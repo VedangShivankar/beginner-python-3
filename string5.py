f5c0928ba530de6b3b4db44cf8ee201c60e4c89d
Vedang Shivankar <142177991+VedangShivankar@users.noreply.github.com>
2024-06-06 14:32:30 -0400

    Add files via upload

---
+++ string5.py
@@ -0,0 +1,19 @@
+text = "ice cream"
+print(text)
+print(text[0])
+print(text[0:3])
+print(text[4:])
+print(text[:3])
+
+address = '''purple street
+NY
+USA'''
+print(address)
+
+
+s = "total states in US "
+num = 50
+str(num) 
+
+total = s + str(num)
+print(total)
